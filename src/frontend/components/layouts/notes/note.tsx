94d17c36feee653c1c1b4480145f9a9ba59b0c96
CrackNicolas <beltrannicolasalejo@gmail.com>
2023-12-04 11:58:42 -0300

    Notes aplication v1.0.0

--- src/frontend/components/layouts/notes/note.tsx
+++ src/frontend/components/layouts/notes/note.tsx
@@ -2,10 +2,15 @@ import ComponentIcon from "../../partials/icon";
 
 export default function ComponentNote() {
     return (
-        <div className="grid grid-cols-9 w-full bg-sixth px-2.5 py-2 cursor-pointer rounded-md">
-            <span className="col-span-8 text-md font-normal tracking-wide text-tertiary">
-                Programar una aplicacion que este al nivel de las solicitudes de mi clientes asas das das das das d asd
-            </span>
+        <div className="grid grid-cols-9 w-full bg-sixth pl-2.5 py-2 cursor-pointer rounded-md">
+            <div className="col-span-8 flex flex-col">
+                <span className="text-md font-normal hover:font-semibold tracking-wide text-secondary">
+                    Titulo
+                </span>
+                <p className="line-clamp-1 text-sm text-tertiary opacity-50 hover:opacity-100 transition duration-700">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod, sunt natus, officiis vitae consectetur, labore reiciendis impedit totam sed porro excepturi! Eius esse itaque aliquid neque repellendus suscipit cum architecto?
+                </p>
+            </div>
             <div className="col-span-1 flex flex-col place-items-center gap-y-2">
                 <span className="text-tertiary text-[11px] opacity-50 px-1.5">
                     Hace 1 min
