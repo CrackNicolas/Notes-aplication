13fcce4f013fbb01e8a89d53637e0f8ab86c693d
CrackNicolas <beltrannicolasalejo@gmail.com>
2024-03-10 23:12:18 -0300

    Notes aplication v1.0.0

--- src/frontend/__test__/partials/nav/page.test.tsx
+++ src/frontend/__test__/partials/nav/page.test.tsx
@@ -1,9 +1,15 @@
-import { render } from "@testing-library/react"
+import '@testing-library/jest-dom'
+
+import { render, screen } from "@testing-library/react"
 
 import ComponentNav from "@/frontend/components/partials/nav/container"
 
 describe('Nav', () => {
     it('Renderizacion correcta', () => {
-        render(<ComponentNav />)
+        render(<ComponentNav section_current="/" user={{}} button_login={<></>} />)
+
+        const link_home = screen.getByTitle('Logo');
+
+        expect(link_home).toBeInTheDocument();
     })
 })
