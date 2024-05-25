06081197e47ee5e12627640f60456da352983934
CrackNicolas <beltrannicolasalejo@gmail.com>
2024-05-25 00:54:44 -0300

    Notes aplication v1.0.0

--- src/frontend/components/partials/nav/top.tsx
+++ src/frontend/components/partials/nav/top.tsx
@@ -28,10 +28,10 @@ export default function ComponentNavTop(props: Props_context) {
                             <ComponentIcon testid="icon-home" name={`${focus ? 'logo-fill' : 'logo'}`} size={27} description_class="icon-home text-secondary cursor-pointer" />
                         </Link>
                         {
-                            (session.user.id !== '') && (
+                            session.user.id !== '' && (
                                 <div className="hidden sm:ml-4 sm:block">
                                     <div className="flex space-x-1">
-                                        <Link href="/dashboard/main" className={`${(section_current === "dashboard") && 'text-secondary'} hover:text-secondary tracking-wider text-fifth px-1 py-2 text-md font-normal transition duration-500`} title="Panel">Panel</Link>
+                                        <Link href="/dashboard/main" className={`${section_current === "dashboard" ? 'text-secondary' : ''} hover:text-secondary tracking-wider text-fifth px-1 py-2 text-md font-normal transition duration-500`} title="Panel">Panel</Link>
                                     </div>
                                 </div>
                             )
@@ -39,7 +39,7 @@ export default function ComponentNavTop(props: Props_context) {
                     </div>
                     <div className="absolute inset-y-0 right-0 flex items-center pr-1 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                         {
-                            (session.user.id !== '') ?
+                            session.user.id !== '' ?
                                 <div className="flex gap-x-4">
                                     <button type="button" title="Notificaciones" className="relative rounded-full p-1 outline-none">
                                         <ComponentIcon name="notification" size={20} description_class="hover:text-secondary text-fifth" />
@@ -56,8 +56,14 @@ export default function ComponentNavTop(props: Props_context) {
                     </div>
                 </div>
             </div>
-            <div title="toggle" className={`bg mx-4 ${view_toggle ? 'visible' : 'hidden'} sm:hidden`}>
-                <div className="space-y-2 px-2 py-2">
+            <div title="toggle" className={`mx-4 transform transition-all duration-500 ease-in-out sm:hidden ${view_toggle ? 'max-h-screen opacity-100 scale-100' : 'max-h-0 opacity-0 scale-95'} overflow-hidden`}>
+                <div className="space-y-2 px-0.5 py-2">
+                    <Link href="/dashboard/main" onClick={() => setView_toggle(false)} className="group flex justify-between items-center border hover:border-secondary rounded-md border-1 px-3 py-2">
+                        <span className="tracking-wider text-fifth text-md font-normal group-hover:text-secondary transition duration-500">
+                            Panel
+                        </span>
+                        <ComponentIcon testid="icon-home" name={`${focus ? 'logo-fill' : 'logo'}`} size={23} description_class="icon-home group-hover:text-secondary text-fifth cursor-pointer transition duration-500" />
+                    </Link>
                     <Link href="/dashboard/main" onClick={() => setView_toggle(false)} className="group flex justify-between items-center border hover:border-secondary rounded-md border-1 px-3 py-2">
                         <span className="tracking-wider text-fifth text-md font-normal group-hover:text-secondary transition duration-500">
                             Panel
@@ -73,5 +79,5 @@ export default function ComponentNavTop(props: Props_context) {
                 </div>
             </div>
         </nav>
-    )
+    );
 }
