90dc706252047e931f7f88863c7ade25c48f3971
CrackNicolas <beltrannicolasalejo@gmail.com>
2024-05-25 01:29:28 -0300

    Notes aplication v1.0.0

--- src/frontend/components/partials/template/dashboard/item.tsx
+++ src/frontend/components/partials/template/dashboard/item.tsx
@@ -2,9 +2,11 @@
 
 import Link from "next/link";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import ComponentIcon from "@/frontend/components/partials/icon";
+
+import { Time_elapsed } from "@/frontend/logic/format_time";
 
 type Props = {
   url: string,
@@ -16,12 +18,22 @@ export default function ComponentItem(props: Props) {
   const { url, title, description } = props;
 
   const [focus, setFocus] = useState<boolean>(false);
+  const [last_time, setLast_time] = useState<string>('Prueba nuestra funcion');
+
+  useEffect(() => {
+    const date = localStorage.getItem(url);
+    setLast_time((date) ? Time_elapsed(new Date(JSON.parse(date))) : 'Prueba nuestra funcion');
+  }, [url])
+
+  const view = () => {
+    localStorage.setItem(url, JSON.stringify(new Date()));
+  }
 
   return (
-    <Link href={url} title={title} className="flex w-full flex-col items-start justify-between bg-sixth sm:px-4 px-3 sm:py-3 py-3 cursor-pointer rounded-md hover:shadow-sm hover:shadow-secondary transition duration-700" onMouseOver={() => setFocus(true)} onMouseLeave={() => setFocus(false)}>
+    <Link href={url} onClick={() => view()} title={title} className="flex w-full flex-col items-start justify-between bg-sixth sm:px-4 px-3 sm:py-3 py-3 cursor-pointer rounded-md hover:shadow-sm hover:shadow-secondary transition duration-700" onMouseOver={() => setFocus(true)} onMouseLeave={() => setFocus(false)}>
       <div className="flex items-center justify-between w-full gap-x-4 text-xs pr-1">
         <span className="text-tertiary opacity-50 hover:opacity-100 transition duration-700">
-          {"Ultima vez "}
+          {last_time.replace('Creada', 'Ultima vez')}
         </span>
         <ComponentIcon name={`${focus ? 'logo-fill' : 'logo'}`} size={20} description_class="text-secondary cursor-pointer" />
       </div>
