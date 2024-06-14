7277dada8a6b5bb9c2a413aa66c3c76739745cc7
CrackNicolas <beltrannicolasalejo@gmail.com>
2024-06-14 17:51:01 -0300

    Notes aplication v1.0.0

--- next.config.js
+++ next.config.js
@@ -17,13 +17,8 @@ const nextConfig = {
         ]
     },
     env: {
-        NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY: "pk_test_cHJldHR5LWtpdGUtNjkuY2xlcmsuYWNjb3VudHMuZGV2JA",
-        CLERK_SECRET_KEY: "sk_test_20kfVy7gI6MgWd9kLV7pwnqbn9aoizSunTu5dM1ZCi",
         NEXT_PUBLIC_CLERK_SIGN_IN_URL: "/sign-in",
-        NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: "/",
-        CLOUDINARY_CLOUD_NAME: "cracknicolas",
-        CLOUDINARY_API_KEY: "716978288343629",
-        CLOUDINARY_API_SECRET: "b0SkQa2gfooFNsGMO5yDNgMBaIM"
+        NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL: "/"
     }
 }
 
