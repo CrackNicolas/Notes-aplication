b58fc7def8947090ad8fbaa606726a8c4740a28f
CrackNicolas <beltrannicolasalejo@gmail.com>
2024-06-14 17:28:58 -0300

    Notes aplication v1.0.0

--- src/backend/utils/db.ts
+++ src/backend/utils/db.ts
@@ -7,7 +7,7 @@ const props = {
 export async function Conect_database(): Promise<boolean> {
     try {
         if (props.isConnected) return true;
-        const db = await connect('mongodb://localhost/notes_aplication');
+        const db = await connect(`${process.env.MONGODB_URL}`);
         props.isConnected = (db.connections[0].readyState === 1)
         return props.isConnected;
     } catch (error) {
