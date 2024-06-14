d47d3644d8b7799df4bf5b7a0e27d0273aa4ed0b
CrackNicolas <beltrannicolasalejo@gmail.com>
2024-06-14 16:53:07 -0300

    Notes aplication v1.0.0

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { authMiddleware } from '@clerk/nextjs';
 
 export default authMiddleware({
-    publicRoutes: ['/', '/favicon.ico']
+    publicRoutes: ['/', '/favicon.ico', '/without_internet']
 });
 
 export const config = {
